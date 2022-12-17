bd375998f96497ad81c4dcc9d32072601043bf96
蔡佳慧 <shenwenjie@dth618.com>
2022-12-17 22:23:01 +0800

    修改部分参数，提交

--- src/main/java/com/caicai/mybatisplusdemo/vo/PlayerInfoVO.java
+++ src/main/java/com/caicai/mybatisplusdemo/vo/PlayerInfoVO.java
@@ -2,12 +2,16 @@ package com.caicai.mybatisplusdemo.vo;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+/**
+ * 一定需要添加@Data注释，因为@Data注释等于为实体类的所有属性添加了
+ * get、set、equals、hashCode、toString等方法
+ * ORM框架将字段值映射到实体类的时候，通过Setter方法为类的属性赋值
+ * 因此，返回值一定需要@Data注解
+ */
 @Data
-@NoArgsConstructor
 public class PlayerInfoVO {
 
     @ApiModelProperty(value = "主键ID")
